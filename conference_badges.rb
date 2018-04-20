741591bbe435f5ab9615fd3cb2ce7a832e002b4c
Hector Ariceaga <hector_ariceaga@hotmail.com>
2018-04-20 19:04:35 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -5,10 +5,13 @@ def badge_maker(name)
 end
 
 def batch_badge_creator(names)
- names.each {|name| badge_maker(name)}
+ names.collect {|name| badge_maker(name)}
 end
 
 def assign_rooms(names)
   names.collect.each_with_index {|name,index| puts "Hello, #{name}! You'll be assigned to room #{index +1}!"
 end
 
+def printer
+
+end
